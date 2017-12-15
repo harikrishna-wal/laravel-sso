662defc13905ee57cfed39b4a430162c2ae31a31
harikrishna-wal <34159536+harikrishna-wal@users.noreply.github.com>
2017-12-15 18:54:40 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //if (!\App::environment('local')) {
-          \URL::forceSchema('https');
-        //}
+        
     }
 
     /**
